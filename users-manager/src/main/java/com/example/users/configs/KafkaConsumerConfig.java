507cf39d6d157f48569f441e0ddc0e6252191f20
Eduardo Cesar <eduardo.borsato.oli@gmail.com>
2025-05-30 19:23:11 -0300

    Add retry kafka message, and commit message after received

--- users-manager/src/main/java/com/example/users/configs/KafkaConsumerConfig.java
+++ users-manager/src/main/java/com/example/users/configs/KafkaConsumerConfig.java
@@ -5,14 +5,20 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.CommonErrorHandler;
+import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.util.backoff.FixedBackOff;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Configuration
 public class KafkaConsumerConfig {
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
@@ -37,10 +43,17 @@ public class KafkaConsumerConfig {
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, UserEventDTO> kafkaListenerContainerFactory() {
+    public ConcurrentKafkaListenerContainerFactory<String, UserEventDTO> kafkaListenerContainerFactory(CommonErrorHandler commonErrorHandler) {
         ConcurrentKafkaListenerContainerFactory<String, UserEventDTO> factory =
                 new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
+
+        DefaultErrorHandler errorHandler = new DefaultErrorHandler(
+                new FixedBackOff(30000L, 5)
+        );
+
+        factory.setCommonErrorHandler(errorHandler);
         return factory;
     }
 }
--- users-manager/src/main/java/com/example/users/events/UserManagerConsumer.java
+++ users-manager/src/main/java/com/example/users/events/UserManagerConsumer.java
@@ -1,26 +1,54 @@
 package com.example.users.events;
 
+import com.example.users.dto.ResponseDTO;
 import com.example.users.dto.UserEventDTO;
+import com.example.users.exception.UserException;
 import com.example.users.external.AccountServiceClient;
+import com.example.users.utils.KafkaEventTrackerInMemory;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
 public class UserManagerConsumer {
-    private final AccountServiceClient accountServiceClient;
 
-    public UserManagerConsumer(AccountServiceClient accountServiceClient) {
+
+    private final AccountServiceClient accountServiceClient;
+    private final KafkaEventTrackerInMemory kafkaEventTracker;
+
+    public UserManagerConsumer(AccountServiceClient accountServiceClient, KafkaEventTrackerInMemory kafkaEventTracker) {
         this.accountServiceClient = accountServiceClient;
+        this.kafkaEventTracker = kafkaEventTracker;
     }
 
     @KafkaListener( topics = "${spring.kafka.topic.name}",
                     groupId = "${spring.kafka.consumer.group-id}",
                     containerFactory = "kafkaListenerContainerFactory")
-    public void consumeEvent(UserEventDTO userEventDTO) {
+    public void consumeEvent(UserEventDTO userEventDTO, Acknowledgment ack) {
         log.info("User event received in account-manager service {}", userEventDTO);
-        accountServiceClient.sendDataAccountService(userEventDTO.getUserDTO());
-        log.info("Reprocessed successfully {}", true);
+        try {
+            ResponseDTO response = accountServiceClient.sendDataAccountService(userEventDTO.getUserDTO());
+
+            boolean success = response != null
+                    && response.data() != null
+                    && !response.data().isEmpty()
+                    && !Boolean.FALSE.equals(response.data().get(0));
+
+            if (success) {
+                kafkaEventTracker.removeSent(userEventDTO.getUserDTO().getIdReference());
+                ack.acknowledge();
+                log.info("Reprocessed successfully and removed idReference");
+            } else {
+                log.warn("Response returned but not successful: {}", response);
+                throw new UserException("Processing failed for user event, triggering retry. Response: " + response);
+            }
+        } catch (Exception e) {
+            log.error("Failed to process user: {}", userEventDTO.getUserDTO(), e);
+            throw e;
+        }
     }
 }
